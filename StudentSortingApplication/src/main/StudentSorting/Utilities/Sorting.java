fabe7329712aa8d51014f40b8e74555bffa5c602
ScottyP666 <63470831+ScottyP666@users.noreply.github.com>
2021-12-31 11:48:44 -0800

    Delete Sorting.java

--- StudentSortingApplication/src/main/StudentSorting/Utilities/Sorting.java
+++
@@ -1,8 +0,0 @@
-
-public interface Sorting {
-	public Student[] convertStudentCSVToArray(String path) throws FileNotFoundException, IOException ;
-	
-	public Student[][] organizeStudentArray(Student[] completeStudentList);
-	
-	public void convertingStudentArrayToFiles(Student[][] theArray) throws IOException;
-}
